db77be23dd401cbb9249661161210c9531aa5b7a
felixabraham670706 <felixabraham670706@gmail.com>
2026-03-16 18:07:08 +0400

    updated app.py with time

--- enbdReddit.py
+++ enbdReddit.py
@@ -62,7 +62,7 @@ except (OAuthException, ResponseException) as e:
 # In[4]:
 
 
-LIMIT = 500   # number of results to fetch per bank
+LIMIT = 50   # number of results to fetch per bank
 DAYS  = 1     # lookback period
 
 
