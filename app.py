2ca90193e218117ed084504c557c23323fcd9b60
felixabraham670706 <felixabraham670706@gmail.com>
2026-03-16 11:27:25 +0400

    updated app.py with time

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
 import os
+from datetime import datetime
+from streamlit_autorefresh import st_autorefresh
+import pytz
 
 st.set_page_config(page_title="ENBD Reddit Dashboard", layout="wide")
 
-st.title("Emirates NBD Reddit Sentiment Dashboard")
+file_time = os.path.getmtime("bank_reddit_posts_last_week.csv")
+
+dubai = pytz.timezone("Asia/Dubai")
+last_update = datetime.fromtimestamp(file_time, dubai)
+st.write("Last data update:", last_update.strftime("%Y-%m-%d %H:%M:%S"))
+
+st_autorefresh(interval=300000)
+#st.title("Emirates NBD Reddit Sentiment Dashboard")
 
 html_file = "ReddIt_weekly_post_analysis.html"
 
